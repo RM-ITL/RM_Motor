3cf3e69215b559a779fa82b5f7f1a4c53576c7e7
yuming <3305708832@qq.com>
2025-06-18 18:47:42 +0800

    third

---
+++ DM_Motor_test/MDK-ARM/DM_Motor_test/startup_stm32h723xx.d
@@ -0,0 +1,1 @@
+dm_motor_test\startup_stm32h723xx.o: startup_stm32h723xx.s
---
+++ DM_Motor_test/MDK-ARM/RTE/_DM_Motor_test/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DM_Motor_test' 
+ * Target:  'DM_Motor_test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM7.h"
+
+
+#endif /* RTE_COMPONENTS_H */
